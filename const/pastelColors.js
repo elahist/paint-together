e3f3ace86242466ac21c8e2179ca0aa3423d8638
elahist <theehtishamelahi@gmail.com>
2025-10-06 19:38:11 +0500

    added pre defined nicks and colors for users

---
+++ const/pastelColors.js
@@ -0,0 +1,14 @@
+export const pastelColors = [
+    "#FFD1DC", // pink
+    "#FFE5B4", // peach
+    "#FFFACD", // lemon
+    "#C1E1C1", // green
+    "#BFFCC6", // mint
+    "#AEC6CF", // blue
+    "#E6E6FA", // lavender
+    "#D8BFD8", // lilac
+    "#FFBCB3", // coral
+    "#AFEEEE", // turquoise
+    "#D3D3D3", // gray
+    "#FFD8B1", // orange
+];
