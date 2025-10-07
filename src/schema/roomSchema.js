ea3f0e7791d0f515e80a847e1039026b772610c3
elahist <theehtishamelahi@gmail.com>
2025-10-07 22:36:38 +0500

    updated schema to be more reliable

--- src/schema/roomSchema.js
+++ src/schema/roomSchema.js
@@ -1,18 +1,41 @@
 import mongoose from "mongoose";
 
+const userSchema = new mongoose.Schema(
+  {
+    ip: { type: String, required: true },
+    socketId: { type: String, required: true },
+    clientID: { type: String, required: true },
+  },
+  { _id: false }
+);
+
 const roomSchema = new mongoose.Schema({
-    roomID: { type: Number, required: true, unique: true },
-    canvas_height: Number,
-    canvas_width: Number,
-    grid_height: Number,
-    grid_width: Number,
-    grid: [[String]],
-    creatorIP: String,
-    creatorToken: { type: String, required: true },
-    users: [String],
-    created_at: { type: Date, default: Date.now },
-    updated_at: { type: Date, default: Date.now },
-    available_at: { type: Date, default: Date.now, required: false },
+  roomID: { type: Number, required: true, unique: true },
+
+  canvas_height: Number,
+  canvas_width: Number,
+  grid_height: Number,
+  grid_width: Number,
+  grid: [[String]],
+
+  creator: {
+    ip: { type: String, required: true },
+    socketId: { type: String, required: true },
+    clientID: { type: String, required: true },
+  },
+
+  creatorToken: { type: String, required: true },
+  users: [userSchema],
+
+  is_available: { type: Boolean, default: true },
+
+  created_at: { type: Date, default: Date.now },
+  updated_at: { type: Date, default: Date.now },
+});
+
+roomSchema.pre("save", function (next) {
+  this.updated_at = new Date();
+  next();
 });
 
 export const Room = mongoose.model("Room", roomSchema);
